d2522e120427d714e88057a15983ab45cfde0d65
ManiyevaSimaCode <116644266+ManiyevaSimaCode@users.noreply.github.com>
2023-02-26 20:07:54 +0400

    Models

---
+++ Models/AccountVM.cs
@@ -0,0 +1,11 @@
+﻿using Entities.DTOs.Account;
+using Entities.DTOs.AppUser;
+
+namespace SimRaMVC.Models
+{
+    public class AccountVM
+    {
+        public AppUserGetDto AppUser { get; set; }
+        public RegisterDto registerDto { get; set; }
+    }
+}
---
+++ Models/HomeVM.cs
@@ -0,0 +1,15 @@
+﻿using Entities.DTOs.Account;
+using Entities.DTOs.Company;
+using Entities.DTOs.Product;
+
+namespace SimRaMVC.Models
+{
+    public class HomeVM
+    {
+        public RegisterDto RegisterDto { get; set; }
+        public LoginDto LoginDto { get; set; }
+        public List<ProductGetDto> Products { get; set; }
+        public ProductGetDto Product { get; set; }
+        public List<CompanyGetDto> Companies { get; set; }
+    }
+}
